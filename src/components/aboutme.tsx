9d65bfcc43dcd0df606d8d50be846f7e0c871b81
Justin <47122570+justintricate@users.noreply.github.com>
2023-03-06 17:20:05 -0600

    all icons added, solidjs still color

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -1,3 +1,13 @@
+import {
+  SiSvelte,
+  SiTypescript,
+  SiNodedotjs,
+  SiReact,
+  SiJavascript,
+} from "solid-icons/si";
+import { TbBrandReactNative } from "solid-icons/tb";
+import { Icon } from "@iconify-icon/solid";
+
 const About = () => {
   return (
     <div class="component">
@@ -22,13 +32,41 @@ const About = () => {
       </p>
       <p>Here's a bit of what I've worked in recently:</p>
       <ul class="about-list">
-        <li>JavaScript (ES6+)</li>
-        <li>React</li>
-        <li>React Native</li>
-        <li>Node.js</li>
-        <li>TypeScript</li>
-        <li>SolidJS</li>
-        <li>Svelte</li>
+        <li>
+          <SiJavascript color="#ccd6f6" style={{ "margin-right": "0.5em" }} />
+          JavaScript (ES6+)
+        </li>
+        <li>
+          <SiReact color="#ccd6f6" style={{ "margin-right": "0.5em" }} />
+          React
+        </li>
+        <li>
+          <TbBrandReactNative
+            color="#ccd6f6"
+            style={{ "margin-right": "0.5em" }}
+          />
+          React Native
+        </li>
+        <li>
+          <SiNodedotjs color="#ccd6f6" style={{ "margin-right": "0.5em" }} />
+          Node.js
+        </li>
+        <li>
+          <SiTypescript color="#ccd6f6" style={{ "margin-right": "0.5em" }} />
+          TypeScript
+        </li>
+        <li>
+          <Icon
+            icon="logos:solidjs-icon"
+            color="#ccd6f6"
+            style={{ "margin-right": "0.5em" }}
+          />
+          SolidJS
+        </li>
+        <li>
+          <SiSvelte color="#ccd6f6" style={{ "margin-right": "0.5em" }} />
+          Svelte
+        </li>
       </ul>
     </div>
   );
