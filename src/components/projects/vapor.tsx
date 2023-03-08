73f26f27260a7cc17cf3d210f79357dd6a333fcb
Justin <47122570+justintricate@users.noreply.github.com>
2023-03-07 19:47:11 -0600

    initial nav styling

--- src/components/projects/vapor.tsx
+++ src/components/projects/vapor.tsx
@@ -72,24 +72,6 @@ const Vapor = () => {
       </div>
     </div>
   );
-
-  return (
-    <div>
-      <h2>Vapor</h2>
-      <p>
-        A single page full stack eCommerce store utilizing the Best Buy API.
-        Allows users to filter and search items, add to cart, and more.
-        Implemented user authentication and password encryption.
-      </p>
-      <ul>
-        <li>React</li>
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>React Redux</li>
-      </ul>
-    </div>
-  );
 };
 
 export default Vapor;
