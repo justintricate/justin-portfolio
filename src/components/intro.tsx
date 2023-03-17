657eb25bb3f237dc237aed0b02d2f3e1818c0e93
Justin <47122570+justintricate@users.noreply.github.com>
2023-03-17 03:56:37 -0500

    adds typewriter, addresses #8

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,11 +1,47 @@
+//@ts-nocheck
+import TypeIt from "typeit";
+import { onCleanup } from "solid-js";
+
 const Intro = () => {
+  let text = [
+    "craft intuitive user experiences.",
+    "optimize for speed and performance.",
+    "find solutions for complex problems.",
+    "prioritize accessibility.",
+    "build mobile-friendly designs.",
+    "explore new technologies.",
+  ];
+
+  const typewriterEl = (el) => {
+    // initialize TypeIt instance
+    const typeit = new TypeIt(el, {
+      strings: text,
+      speed: 85,
+      lifeLike: false,
+      breakLines: false,
+      cursorChar: "|",
+      waitUntilVisible: true,
+      nextStringDelay: [3000, 750],
+      loop: true,
+    }).go();
+
+    onCleanup(() => {
+      typeit.destroy();
+    });
+  };
+
   return (
     <div class="intro-container">
       <p class="monogreen">Hello! My name is</p>
       <h1>
         <div>
           <p class="name-style">Justin Wooley</p>
-          <p class="subheader-style">I like to build things for the web.</p>
+          <p class="subheader-style">
+            I like to{" "}
+            <span id="typewriter" ref={typewriterEl}>
+              build things for the web.
+            </span>
+          </p>
         </div>
       </h1>
       <p class="intro-p-style">
