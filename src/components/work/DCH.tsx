63764774a9fe87af4f9e0b06732e2f1e886a48e7
Justin <47122570+justintricate@users.noreply.github.com>
2024-07-04 12:46:07 -0700

    Create DCH.tsx
    created DCH job page

---
+++ src/components/work/DCH.tsx
@@ -0,0 +1,26 @@
+const DCH = () => {
+  return (
+    <>
+      <h2>
+        Patient Gaming Specialist <span class="at-symbol">@</span> Doernbecher Children's Hospital
+      </h2>
+      <h3>2024 - Current</h3>
+      <ul>
+        <li>
+          Develop, plan, and implement activities utilizing gaming and other technologies to enhance wellbeing of pediatric patients.
+        </li>
+        <li>
+          Manage gaming equipment for patients, as well as other teams within the hospital-- to include technical and practical support.
+        </li>
+        <li>
+          Provide bedside play with patients directly, offering a sense of normalcy and escapism through video games and other technology such as VR.
+        </li>
+        <li>
+          Manage inventory across the entire hospital of all carts, consoles, games, profiles, and gaming accessories.
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default DCH;
